c8c0cf602dc4b36e0ce7f5c0159d7b1b976d6c54
jeferreirajf <jeferreirajf@gmail.com>
2022-10-21 12:14:33 -0300

    Adicao da pagina home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,10 @@
 import "./style.css";
 
-export function Home(){
+export function Home (){
     return (
         <div className="home">
             <h1 className="home">HOME</h1>
-            <div className="home-content">Conteudo vem aqui</div>
+            <div> CONTEUDO </div>
         </div>
     );
 }
