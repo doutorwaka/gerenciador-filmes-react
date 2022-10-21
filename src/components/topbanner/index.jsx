56d91f5e1485df545ffe8ff046f9622e977d7fe9
jeferreirajf <jeferreirajf@gmail.com>
2022-10-21 09:48:43 -0300

    Adicao de logo responsivo no topbanner

--- src/components/topbanner/index.jsx
+++ src/components/topbanner/index.jsx
@@ -1,8 +1,10 @@
+import DoutorWakaLogo from "./imgs/logo.png";
 import "./style.css";
 
 export function Topbanner(){
     return(
         <div className="topbanner">
+            <img src={DoutorWakaLogo} alt="Dr. Waka" className="topbanner"/>
             <center className="topbanner">GERENCIADOR DE FILMES</center>
         </div>
     );
