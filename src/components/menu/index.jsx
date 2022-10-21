4e004122bd2109b90ed374ad0562bbe1a9cd94a1
jeferreirajf <jeferreirajf@gmail.com>
2022-10-21 10:48:10 -0300

    Insercao de LINKS e estilizacao do menu

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
 import "./style.css";
 
 export function Menu(){
     return(
-        <div>
-            HOME
+        <div className="menu">
+            <nav className="menu">
+                <Link to="/" className="menu">HOME</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">FILMES</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">DIRETORES</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">ATORES</Link>
+            </nav>
         </div>
     );
 }
