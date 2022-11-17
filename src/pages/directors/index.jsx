5b8691ecf6bc21394e9c07a568c42dde533873ce
jeferreirajf <jeferreirajf@gmail.com>
2022-11-17 10:44:00 -0300

    Trazendo os filmes e os diretores

--- src/pages/directors/index.jsx
+++ src/pages/directors/index.jsx
@@ -1,10 +1,47 @@
 import "./style.css";
+import axios from "axios";
+import {useState, useEffect} from "react";
+import { BACKEND_URL } from "../../config";
+
+function directorsHandler(directors){
+    
+    if(!Array.isArray(directors)){
+        return directors;
+    }
+    
+    var i  = 0;
+
+    var content = directors.map(function(director){
+        return(
+            <div className="directors-content" key={i++}>{i+1} - {director.nome}</div>
+        );
+    });
+
+    return content;
+}
 
 export function Directors(){
+
+    const [directors, setDirectors] = useState("Nada a mostrar");
+
+    useEffect(function(){
+        axios.get(BACKEND_URL + "/diretores")
+        .then(function (response){
+            console.log("RODADNO... ");
+            setDirectors(directorsHandler(response.data));
+        })
+        .catch(function(error){
+            console.log("Error: " + error);
+        })
+        .finally(function(){
+            console.log("Finally.");
+        });
+    }, []);
+
     return (
         <div className="content">
             <h1>DIRETORES</h1>
-            <div> CONTEUDO </div>
+            {directors}
         </div>
     );
 }
--- src/pages/films/index.jsx
+++ src/pages/films/index.jsx
@@ -1,10 +1,47 @@
 import "./style.css";
+import axios from "axios";
+import {useState, useEffect} from "react";
+import { BACKEND_URL } from "../../config";
+
+function filmsHandler(films){
+    
+    if(!Array.isArray(films)){
+        return films;
+    }
+    
+    var i  = 0;
+
+    var content = films.map(function(film){
+        return(
+            <div className="film-content" key={i++}>{i+1} - {film.nome}</div>
+        );
+    });
+
+    return content;
+}
 
 export function Films (){
+
+    const [films, setFilms] = useState("Nada a mostrar");
+
+    useEffect(function(){
+        axios.get(BACKEND_URL + "/filmes")
+        .then(function (response){
+            console.log("RODADNO... ");
+            setFilms(filmsHandler(response.data));
+        })
+        .catch(function(error){
+            console.log("Error: " + error);
+        })
+        .finally(function(){
+            console.log("Finally.");
+        });
+    }, []);
+
     return (
         <div className="content">
             <h1>FILMES</h1>
-            <div> CONTEUDO </div>
+            {films}
         </div>
     );
 }
