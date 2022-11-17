a50bc280098ed76c2002851707474064cbbbe56f
jeferreirajf <jeferreirajf@gmail.com>
2022-11-17 09:43:03 -0300

    Configuracao da variavel de ambiente.

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,6 +1,10 @@
 import "./style.css";
+import {BACKEND_URL} from "../../config";
 
 export function Actors(){
+
+    console.log("Backend: " + BACKEND_URL);
+
     return (
         <div className="content">
             <h1>ATORES</h1>
