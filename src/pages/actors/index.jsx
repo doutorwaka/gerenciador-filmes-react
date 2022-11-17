c042fee84eb401e8c73a818307f80763aafa56bd
jeferreirajf <jeferreirajf@gmail.com>
2022-11-17 10:35:41 -0300

    Utilizando useEffect

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,14 +1,47 @@
 import "./style.css";
 import {BACKEND_URL} from "../../config";
+import axios from "axios";
+import {useState, useEffect} from "react";
+
+function actorsHandler(actors){
+    
+    if(!Array.isArray(actors)){
+        return actors;
+    }
+    
+    var i  = 0;
+
+    var content = actors.map(function(actor){
+        return(
+            <div className="actors-content" key={i++}>{i+1} - {actor.nome}</div>
+        );
+    });
+
+    return content;
+}
 
 export function Actors(){
 
-    console.log("Backend: " + BACKEND_URL);
+    const [actors, setActors] = useState("Nada a mostrar");
+
+    useEffect(function(){
+        axios.get(BACKEND_URL + "/atores")
+        .then(function (response){
+            console.log("RODADNO... ");
+            setActors(actorsHandler(response.data));
+        })
+        .catch(function(error){
+            console.log("Error: " + error);
+        })
+        .finally(function(){
+            console.log("Finally.");
+        });
+    }, []);
 
     return (
         <div className="content">
             <h1>ATORES</h1>
-            <div> CONTEUDO </div>
+            {actors}
         </div>
     );
 }
