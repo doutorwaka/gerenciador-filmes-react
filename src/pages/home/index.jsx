2d74bf08244380a2135a67be2e567a6c72d9b912
jeferreirajf <jeferreirajf@gmail.com>
2022-11-18 11:06:28 -0300

    Funcionamento do incremento/decremento de elenco.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,36 @@
 import "./style.css";
 import { BsFilePlusFill as PlusIcon } from "react-icons/bs";
 import { BsFileMinusFill as MinusIcon } from "react-icons/bs";
+import {useState} from "react";
+
+function decreaseCastSize(castSize, setCastSize){
+    if(castSize > 1)
+        setCastSize(castSize-1);
+}
+
+function createCastInputs(castSize, setCastSize){
+
+    var content = [];
+
+    for(var i = 0; i < castSize; i++){
+        content.push( 
+            (
+                <p className="home-form">
+                    <div><input type="text" className="home-form" size={40} placeholder="Nome do ator"/></div>
+                    <div><MinusIcon size={25} className="home-form-icon" onClick={()=>decreaseCastSize(castSize, setCastSize)}/></div>
+                    <div><PlusIcon size={25} className="home-form-icon" onClick={()=>setCastSize(castSize+1)}/></div>
+                </p>
+            ) 
+        );
+    }
+
+    return content;
+}
 
 export function Home (){
+
+    const [castSize, setCastSize] = useState(1);
+
     return (
         <div className="content">
             <h1>HOME</h1>
@@ -14,11 +42,8 @@ export function Home (){
                 <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nota"/></p>
                 
                 <h3 className="home-form">Elenco:</h3>
-                <p className="home-form">
-                    <div><input type="text" className="home-form" size={40} placeholder="Nome do ator"/></div>
-                    <div><MinusIcon size={25} className="home-form-icon"/></div>
-                    <div><PlusIcon size={25} className="home-form-icon" /></div>
-                </p>
+                
+                {createCastInputs(castSize, setCastSize)}
             </div>
         </div>
     );
