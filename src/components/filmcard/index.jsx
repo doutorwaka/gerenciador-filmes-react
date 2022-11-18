060dc41678afbeb3f2c94e94b23a1d680359ea39
jeferreirajf <jeferreirajf@gmail.com>
2022-11-18 09:47:37 -0300

    Revert "Revert "Estilizacao final dos filmcards""
    This reverts commit b1032d47e8297f9570203b011c3b9befb09f6b51.

--- src/components/filmcard/index.jsx
+++ src/components/filmcard/index.jsx
@@ -6,10 +6,14 @@ export function Filmcard(props){
 
     return (
         <div className="filmcard">
-            <h2>{props.title}</h2>
-            <div>{props.director}</div>
-            <div>{props.cast}</div>
-            <div>{props.grade}</div>
+            <h2 className="filmcard">{props.title}</h2>
+            <div className="filmcard-content">
+                <div>
+                    <div><b>Diretor: </b> {props.director}</div>
+                    <div><b>Elenco: </b>{props.cast}</div>
+                </div>
+                <div className="filmcard-grade">{props.grade}</div>
+            </div>            
         </div>
     );
 }
