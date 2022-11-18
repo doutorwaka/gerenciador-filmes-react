0863a184c828677b95be9455bd02e007c83bcc59
jeferreirajf <jeferreirajf@gmail.com>
2022-11-18 10:41:25 -0300

    Inicio do formulario de cadastro de filmes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,16 @@ export function Home (){
     return (
         <div className="content">
             <h1>HOME</h1>
-            <div> CONTEUDO </div>
+
+            <div className="home-form">
+                <h3 className="home-form">Inserir novo filme:</h3>
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nome do filme"/></p>
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Diretor"/></p>
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nota"/></p>
+                
+                <h3 className="home-form">Elenco:</h3>
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nome do ator"/></p>
+            </div>
         </div>
     );
 }
