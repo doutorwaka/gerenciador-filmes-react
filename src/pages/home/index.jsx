ceae096db9c33e34a6794f285b5bfa4cb5aee6c6
jeferreirajf <jeferreirajf@gmail.com>
2022-11-18 10:52:54 -0300

    Botoes de mais e menos no elenco.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,6 @@
 import "./style.css";
+import { BsFilePlusFill as PlusIcon } from "react-icons/bs";
+import { BsFileMinusFill as MinusIcon } from "react-icons/bs";
 
 export function Home (){
     return (
@@ -12,7 +14,11 @@ export function Home (){
                 <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nota"/></p>
                 
                 <h3 className="home-form">Elenco:</h3>
-                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nome do ator"/></p>
+                <p className="home-form">
+                    <div><input type="text" className="home-form" size={40} placeholder="Nome do ator"/></div>
+                    <div><MinusIcon size={25} className="home-form-icon"/></div>
+                    <div><PlusIcon size={25} className="home-form-icon" /></div>
+                </p>
             </div>
         </div>
     );
