85b881b0d4131dcf46808d768cf7f6ad8a56c127
jeferreirajf <jeferreirajf@gmail.com>
2023-05-02 11:39:58 -0300

    Atualização de commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,48 +1,48 @@
 import "./style.css";
 import { BsFilePlusFill as PlusIcon } from "react-icons/bs";
 import { BsFileMinusFill as MinusIcon } from "react-icons/bs";
-import {useState} from "react";
+import { useState } from "react";
 
-function decreaseCastSize(castSize, setCastSize){
-    if(castSize > 1)
-        setCastSize(castSize-1);
+function decreaseCastSize(castSize, setCastSize) {
+    if (castSize > 1)
+        setCastSize(castSize - 1);
 }
 
-function createCastInputs(castSize, setCastSize){
+function createCastInputs(castSize, setCastSize) {
 
     var content = [];
 
-    for(var i = 0; i < castSize; i++){
-        content.push( 
+    for (var i = 0; i < castSize; i++) {
+        content.push(
             (
                 <p className="home-form">
-                    <div><input type="text" className="home-form" size={40} placeholder="Nome do ator"/></div>
-                    <div><MinusIcon size={25} className="home-form-icon" onClick={()=>decreaseCastSize(castSize, setCastSize)}/></div>
-                    <div><PlusIcon size={25} className="home-form-icon" onClick={()=>setCastSize(castSize+1)}/></div>
+                    <div><input type="text" className="home-form" size={40} placeholder="Nome do ator" /></div>
+                    <div><MinusIcon size={25} className="home-form-icon" onClick={() => decreaseCastSize(castSize, setCastSize)} /></div>
+                    <div><PlusIcon size={25} className="home-form-icon" onClick={() => setCastSize(castSize + 1)} /></div>
                 </p>
-            ) 
+            )
         );
     }
 
     return content;
 }
 
-export function Home (){
+export function Home() {
 
     const [castSize, setCastSize] = useState(1);
 
     return (
         <div className="content">
             <h1>HOME</h1>
-
+            mensagem vai aqui
             <div className="home-form">
                 <h3 className="home-form">Inserir novo filme:</h3>
-                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nome do filme"/></p>
-                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Diretor"/></p>
-                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nota"/></p>
-                
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nome do filme" /></p>
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Diretor" /></p>
+                <p className="home-form"><input type="text" className="home-form" size={40} placeholder="Nota" /></p>
+
                 <h3 className="home-form">Elenco:</h3>
-                
+
                 {createCastInputs(castSize, setCastSize)}
             </div>
         </div>
